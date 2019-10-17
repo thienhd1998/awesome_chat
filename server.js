c935b254af86152b9a7c22e529102b6da3b4c444
thienhd2598@gmail.com <thienhd2598@gmail.com>
2019-10-17 19:34:30 +0700

    [1.0.0] Add BabelJS.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require("express");
-var app = express();
+import express from "express";
+let app = express();
 
-var hostname = "localhost";
-var port = 8017;
+let hostname = "localhost";
+let port = 8017;
 
 app.get("/helloworld", (req, res) => {
-    res.send("<h1>Hello World</h1>");
+  res.send("<h1>Hello World</h1>");
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Hello Thien,I'm running at ${hostname}:${port}/`);
+  console.log(`Hello Thien,I'm running at ${hostname}:${port}/`);
 });
