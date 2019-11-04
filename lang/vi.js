10e752357922b38893d46b36704560a06f25b765
thienhd2598@gmail.com <thienhd2598@gmail.com>
2019-11-04 19:15:11 +0700

    [Register user pass 01]

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidation = {
+  email_incorrect: "Email phaỉ có dạng example@nkthien.com!",
+  gender_incorrect: "Giới tính thứ 3 ????",
+  password_incorrect: "Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, chữ số và ký tự đặc biệt",
+  password_confirmation_incorrect: "Nhập lại mật khẩu chưa chính xác"
+};
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,25 @@
+import {validationResult} from "express-validator/check";
+
 let getLoginRegister = (req, res) => {
   return res.render("auth/master");
 }; 
 
-let getLogout = (req, res) => {
-  //do something
+let postRegister = (req, res) => {
+  let errorArr = [];
+
+  let validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    let errors = Object.values(validationErrors.mapped());
+    errors.forEach(item => {
+      errorArr.push(item.msg);
+    });
+    console.log(errorArr);
+    return;
+  }
+  console.log(req.body);
 };
 
 module.exports = {
   getLoginRegister: getLoginRegister,
-  getLogout: getLogout
+  postRegister: postRegister
 };
