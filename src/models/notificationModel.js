801b2f7d282814814b5e06679f03b358c3d286a8
thienhd1998 <thienhd2510@gmail.com>
2020-04-27 22:35:07 +0700

    [1.0.0] Count notification design modal

--- src/models/notificationModel.js
+++ src/models/notificationModel.js
@@ -16,7 +16,7 @@ NotificationSchema.statics = {
   },
 
   removeRequestContactNotification(senderId, receiverId, type) {
-    return this.remove({
+    return this.deleteMany({
       $and: [
         {"senderId": senderId},
         {"receiverId": receiverId},
@@ -32,6 +32,19 @@ NotificationSchema.statics = {
    */
   getByUserIdAndLimit(userId, limit) {
     return this.find({"receiverId": userId}).sort({"createdAt": -1}).limit(limit).exec();
+  },
+
+  /**
+   * Count all notifications unread
+   * @param {string} userId 
+   */
+  countNotifUnread(userId) {
+    return this.countDocuments({
+      $and: [
+        {"receiverId": userId},
+        {"isRead": false}
+      ]
+    }).exec();
   }
 }  
 
@@ -43,15 +56,15 @@ const NOTIFICATION_CONTENTS = {
   getContent: (notificationType, isRead, userId, username, userAvatar) => {
     if (notificationType === NOTIFICATION_TYPES.ADD_CONTACT) {
       if (!isRead) {
-        return `<span data-uid="${userId}">
-                <img class="notif-readed-false" src="images/users/${userAvatar}" alt=""> 
+        return `<div class="notif-readed-false" data-uid="${userId}">
+                <img class="avatar-small" src="images/users/${userAvatar}" alt=""> 
                 <strong>${username}</strong> đã gửi cho bạn một lời mời kết bạn!
-                </span><br><br><br>`;
+                </div>`;
       }
-      return `<span data-uid="${userId}">
+      return `<div data-uid="${userId}">
               <img class="avatar-small" src="images/users/${userAvatar}" alt=""> 
               <strong>${username}</strong> đã gửi cho bạn một lời mời kết bạn!
-              </span><br><br><br>`;
+              </div>`;
     }
     return "No matching with any notification type";
   }
